f08269f58cde99f8602471dc1bc1617ae38f3fe9
Juan Ossa <jossaq@amazon.com>
2020-12-17 15:36:14 -0500

    fix typescript

--- src/handler.ts
+++ src/handler.ts
@@ -1,10 +1,5 @@
-console.log('Loading function');
-
-exports.handler = async (event, context) => {
-  //console.log('Received event:', JSON.stringify(event, null, 2));
-  console.log('value1 =', event.key1);
-  console.log('value2 =', event.key2);
-  console.log('value3 =', event.key3);
-  return event.key1; // Echo back the first key value
-  // throw new Error('Something went wrong');
+export const handler = async (event: any = {}): Promise<any> => {
+  console.log('Hello World!');
+  const response = JSON.stringify(event, null, 2);
+  return response;
 };
