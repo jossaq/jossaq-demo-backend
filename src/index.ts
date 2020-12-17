8a92431bad6c17ac43aef363702716ba1fbf4c0b
Juan Ossa <jossaq@amazon.com>
2020-12-17 15:25:18 -0500

    fix lambda location

--- src/index.ts
+++ src/index.ts
@@ -13,9 +13,9 @@ export class BackendProcessor extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string) {
     super(scope, id);
 
-    const entry = fs.existsSync(path.join(__dirname, 'lambda/handler.js'))
-      ? path.join(__dirname, 'lambda/handler.js') // local development
-      : path.join(__dirname, 'lambda/handler.js'); // when published in npm
+    const entry = fs.existsSync(path.join(__dirname, 'handler.js'))
+      ? path.join(__dirname, 'handler.js') // local development
+      : path.join(__dirname, 'handler.js'); // when published in npm
 
 
     this.handler = new lambdanode.NodejsFunction(this, 'backend-processor', {
