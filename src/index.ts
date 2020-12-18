447495a25c7cbe5587b253553c4ba7d592e2fe55
Juan Ossa <jossaq@amazon.com>
2020-12-18 10:20:21 -0500

    feature(dynamo): added table in construct

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import * as dynamodb from '@aws-cdk/aws-dynamodb';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as lambdanode from '@aws-cdk/aws-lambda-nodejs';
 import * as cdk from '@aws-cdk/core';
@@ -13,16 +14,26 @@ export class BackendProcessor extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string) {
     super(scope, id);
 
-    const entry = fs.existsSync(path.join(__dirname, 'handler.ts'))
-      ? path.join(__dirname, 'handler.ts') // local development
-      : path.join(__dirname, 'handler.ts'); // when published in npm
+    const entry = fs.existsSync(path.join(__dirname, 'lambda/handler.ts'))
+      ? path.join(__dirname, 'lambda/handler.ts') // local development
+      : path.join(__dirname, 'lambda/handler.js'); // when published in npm
 
+    const table =new dynamodb.Table(this, 'backend-processor-table', {
+      partitionKey: { name: 'PK', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'SK', type: dynamodb.AttributeType.STRING },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
 
     this.handler = new lambdanode.NodejsFunction(this, 'backend-processor', {
       entry: entry,
       timeout: cdk.Duration.seconds(2),
       runtime: lambda.Runtime.NODEJS_12_X,
       tracing: lambda.Tracing.ACTIVE,
+      environment: {
+        TABLE_NAME: table.tableName,
+      },
       bundling: {
         minify: true,
         externalModules: [
@@ -30,5 +41,7 @@ export class BackendProcessor extends cdk.Construct {
         ],
       },
     });
+
+
   }
 }
--- src/lambda/handler.ts
+++ src/lambda/handler.ts
@@ -1,10 +1,45 @@
-console.log('Loading function');
 
-exports.handler = async (event, context) => {
-  //console.log('Received event:', JSON.stringify(event, null, 2));
-  console.log('value1 =', event.key1);
-  console.log('value2 =', event.key2);
-  console.log('value3 =', event.key3);
-  return event.key1; // Echo back the first key value
-  // throw new Error('Something went wrong');
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.TABLE_NAME;
+
+exports.handler = async (event: any, context: any) => {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+  console.log('Received event:', JSON.stringify(context, null, 2));
+
+  let body;
+  let statusCode = '200';
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    switch (event.httpMethod) {
+      case 'DELETE':
+        body = await dynamo.delete(JSON.parse(event.body)).promise();
+        break;
+      case 'GET':
+        body = await dynamo.scan({ TableName: tableName }).promise();
+        break;
+      case 'POST':
+        body = await dynamo.put(JSON.parse(event.body)).promise();
+        break;
+      case 'PUT':
+        body = await dynamo.update(JSON.parse(event.body)).promise();
+        break;
+      default:
+        throw new Error(`Unsupported method "${event.httpMethod}"`);
+    }
+  } catch (err) {
+    statusCode = '400';
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
 };
